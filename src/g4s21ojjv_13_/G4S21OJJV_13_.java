ce4fd195b9f2e7db640eeee3a4e47abb709c31c1
Lenovo <Lenovo@DESKTOP-S4GDU1Q>
2022-05-21 16:56:35 -0500

    Trabajo 6

---
+++ src/g4s21ojjv_13_/G4S21OJJV_13_.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21ojjv_13_;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class G4S21OJJV_13_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JIFPrincipal jIFPrincipal = new JIFPrincipal();
+        jIFPrincipal.setVisible(true);
+    }
+    
+}
